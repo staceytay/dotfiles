4634079215d38e1ee6db944bd0c9d4ed12702280
Stacey Tay <stacey.tay@shopify.com>
2020-12-11 15:35:56 +0800

    Update oh-my-zsh install

--- osx_setup.sh
+++ osx_setup.sh
@@ -13,7 +13,7 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Create Respositories folder
 mkdir Repositories
